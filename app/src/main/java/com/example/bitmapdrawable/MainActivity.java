6cbbe04c4166c4a1205f19988caffe2e6209b043
tux <rubenoctavio@itsn.edu.mx>
2021-09-21 12:41:33 -0500

    se agregaron todos los archivos al repositorio

---
+++ app/src/main/java/com/example/bitmapdrawable/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bitmapdrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Drawable miImagen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
